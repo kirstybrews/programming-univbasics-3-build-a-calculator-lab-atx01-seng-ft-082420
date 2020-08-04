6adc5be89b507b5757f8e00b6d09f0d0aa6e3c89
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-04 17:47:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,7 +9,8 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
- 
+ quotient = num1 / num2
+ quotient
 end
 
 def multiplication(num1, num2)
@@ -18,7 +19,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+ 
 end
 
 def square_root(num)
