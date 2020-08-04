c285487606fa0f4948d9c5c137a110c588cdc6c0
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-04 17:46:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,19 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
+ sum
 end
 
 def subtraction(num1, num2)
-
+ difference = num1 - num2
+ difference
 end
 
 def division(num1, num2)
-
+ 
 end
 
 def multiplication(num1, num2)
-
+ 
 end
 
 def modulo(num1, num2)
